d5c1bb65a80fd83faee53be70c10f5e3cf9247e6
Lars Roettig <l.roettig@techdivision.com>
2019-02-19 23:51:07 +0100

    24: [New Rule] Static methods SHOULD NOT be used

---
+++ Magento/Sniffs/Functions/StaticFunctionSniff.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sniffs\Functions;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+/**
+ * Detects static function definitions.
+ */
+class StaticFunctionSniff implements Sniff
+{
+    /**
+     * String representation of warning.
+     *
+     * @var string
+     */
+    protected $warningMessage = 'The use of `static` methods is discouraged';
+
+    /**
+     * Warning violation code.
+     *
+     * @var string
+     */
+    protected $warningCode = 'StaticFunction';
+
+    /**
+     * @inheritDoc
+     */
+    public function register()
+    {
+        return [T_STATIC];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $posOfFunction = $phpcsFile->findNext(T_FUNCTION, $stackPtr) + 1;
+        $tokens = array_slice($phpcsFile->getTokens(), $stackPtr, $posOfFunction - $stackPtr);
+
+        $allowedTypes = [T_STATIC => true, T_WHITESPACE => true, T_FUNCTION => true];
+
+        foreach ($tokens as $token) {
+
+            $code = $token['code'];
+            if (!array_key_exists($code, $allowedTypes)) {
+                break;
+            }
+
+            if ($code === T_FUNCTION) {
+                $phpcsFile->addWarning($this->warningMessage, $posOfFunction, $this->warningCode);
+            }
+        }
+    }
+}
