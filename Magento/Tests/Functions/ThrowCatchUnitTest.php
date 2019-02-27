56ef04a4afffaff60fbd6420825e18e6f3beab5d
Lars Roettig <l.roettig@techdivision.com>
2019-02-28 00:03:29 +0100

    #29: [New Rule] Exceptions must not be handled in the same function where they are thrown

---
+++ Magento/Tests/Functions/ThrowCatchUnitTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Tests\Functions;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Class ThrowCatchUnitTest
+ */
+class ThrowCatchUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getWarningList()
+    {
+        return [
+            41 => 1,
+            120 => 1,
+            126 => 1,
+            145 => 1,
+            156 => 1
+        ];
+    }
+}
