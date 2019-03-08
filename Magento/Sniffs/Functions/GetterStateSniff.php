51fdf683c9172a05d15920db490fdcbed2714730
Lars Roettig <l.roettig@techdivision.com>
2019-03-08 14:16:59 +0100

    #26: Add Sniff for Gettersnot change state

---
+++ Magento/Sniffs/Functions/GetterStateSniff.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sniffs\Functions;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
+
+/**
+ * Detects getter that change the state of an object.
+ */
+class GetterStateSniff implements Sniff
+{
+
+    /**
+     * String representation of warning.
+     *
+     * @var string
+     */
+    protected $warningMessage = 'Getters SHOULD NOT change the state of an object.';
+
+    /**
+     * Warning violation code.
+     *
+     * @var string
+     */
+    protected $warningCode = 'GetterState';
+
+    /**
+     * @inheritDoc
+     */
+    public function register()
+    {
+        return [T_FUNCTION];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $methodName = $phpcsFile->getDeclarationName($stackPtr);
+
+        if ($methodName === null || strpos($methodName, 'get') === false) {
+            // Ignore closures and no getters
+            return;
+        }
+
+        $tokens = $phpcsFile->getTokens();
+        $open = $tokens[$stackPtr]['scope_opener'];
+        $close = $tokens[$stackPtr]['scope_closer'];
+
+        $isObjectScope = false;
+        $isObjectScopeToken = [T_SELF => T_SELF, T_PARENT => T_PARENT, T_STATIC => T_STATIC];
+
+        for ($i = ($open + 1); $i < $close; $i++) {
+            $token = $tokens[$i];
+
+            if (T_SEMICOLON === $token['code']) {
+                // Detect line end scope change to function scope.
+                $isObjectScope = false;
+            }
+
+            if (array_key_exists($token['code'], $isObjectScopeToken)) {
+                $isObjectScope = true;
+            }
+
+            if ($token['content'] === '$this') {
+                $isObjectScope = true;
+            }
+
+            if ($isObjectScope === true && array_key_exists($token['code'], Tokens::$assignmentTokens)) {
+                $phpcsFile->addWarning($this->warningMessage, $i, $this->warningCode);
+            }
+        }
+    }
+}
---
+++ Magento/Tests/Functions/GetterStateUnitTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Tests\Functions;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Class GetterStateUnitTest
+ */
+class GetterStateUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getWarningList()
+    {
+        return [
+            28 => 1,
+            29 => 1,
+            30 => 1,
+            39 => 1,
+        ];
+    }
+}
