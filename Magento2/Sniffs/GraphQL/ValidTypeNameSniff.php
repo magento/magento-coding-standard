8cb90c89e32224b641beed3c42493be61174f836
Jean-Bernard Valentaten <j.valentaten@techdivision.com>
2019-09-09 17:06:02 +0200

    MC-19366: Improves GraphQL tokenizer by making use of webonyx/graphql-php library

--- Magento2/Sniffs/GraphQL/ValidTypeNameSniff.php
+++ Magento2/Sniffs/GraphQL/ValidTypeNameSniff.php
@@ -5,13 +5,15 @@
  */
 namespace Magento2\Sniffs\GraphQL;
 
-use PHP_CodeSniffer\Standards\Squiz\Sniffs\Classes\ValidClassNameSniff;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Util\Common;
 
 /**
  * Detects types (<kbd>type</kbd>, <kbd>interface</kbd> and <kbd>enum</kbd>) that are not specified in
  * <kbd>UpperCamelCase</kbd>.
  */
-class ValidTypeNameSniff extends ValidClassNameSniff
+class ValidTypeNameSniff implements Sniff
 {
 
     /**
@@ -26,7 +28,35 @@ class ValidTypeNameSniff extends ValidClassNameSniff
      */
     public function register()
     {
-        return [T_CLASS];
+        return [T_CLASS, T_INTERFACE];
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        //compose entity name by making use of the next strings the we find until we hit a non-string token
+        $name = '';
+        for ($i=$stackPtr+1; $tokens[$i]['code'] === T_STRING; ++$i) {
+            $name .= $tokens[$i]['content'];
+        }
+
+        $valid = Common::isCamelCaps($name, true, true, false);
+
+        if ($valid === false) {
+            $type  = ucfirst($tokens[$stackPtr]['content']);
+            $error = '%s name "%s" is not in PascalCase format';
+            $data  = [
+                $type,
+                $name,
+            ];
+            $phpcsFile->addError($error, $stackPtr, 'NotCamelCaps', $data);
+            $phpcsFile->recordMetric($stackPtr, 'PascalCase class name', 'no');
+        } else {
+            $phpcsFile->recordMetric($stackPtr, 'PascalCase class name', 'yes');
+        }
+    }
 }
--- PHP_CodeSniffer/Tokenizers/GraphQL.php
+++ PHP_CodeSniffer/Tokenizers/GraphQL.php
@@ -6,19 +6,63 @@
 
 namespace PHP_CodeSniffer\Tokenizers;
 
+use GraphQL\Language\Lexer;
+use GraphQL\Language\Source;
+use GraphQL\Language\Token;
 use PHP_CodeSniffer\Config;
+use PHP_CodeSniffer\Exceptions\TokenizerException;
 
 /**
  * Implements a tokenizer for GraphQL files.
+ *
+ * @todo Reimplement using the official GraphQL implementation
  */
-class GraphQL extends JS
+class GraphQL extends Tokenizer
 {
 
-    protected $additionalTokenValues = [
-        'type'      => 'T_CLASS',
-        'interface' => 'T_CLASS',
-        'enum'      => 'T_CLASS',
-        '#'         => 'T_COMMENT',
+    /**
+     * Defines how GraphQL token types are mapped to PHP token types.
+     *
+     * @var array
+     */
+    private $tokenTypeMap = [
+        Token::AMP          => null, //TODO
+        Token::AT           => 'T_DOC_COMMENT_TAG',
+        Token::BANG         => null, //TODO
+        Token::BLOCK_STRING => 'T_COMMENT',
+        Token::BRACE_L      => 'T_OPEN_CURLY_BRACKET',
+        Token::BRACE_R      => 'T_CLOSE_CURLY_BRACKET',
+        Token::BRACKET_L    => 'T_OPEN_SQUARE_BRACKET',
+        Token::BRACKET_R    => 'T_CLOSE_CURLY_BRACKET',
+        Token::COLON        => 'T_COLON',
+        Token::COMMENT      => 'T_COMMENT',
+        Token::DOLLAR       => 'T_DOLLAR',
+        Token::EOF          => 'T_CLOSE_TAG',
+        Token::EQUALS       => 'T_EQUAL',
+        Token::FLOAT        => null, //TODO
+        Token::INT          => null, //TODO
+        Token::NAME         => 'T_STRING',
+        Token::PAREN_L      => 'T_OPEN_PARENTHESIS',
+        Token::PAREN_R      => 'T_CLOSE_PARENTHESIS',
+        Token::PIPE         => null, //TODO
+        Token::SPREAD       => 'T_ELLIPSIS',
+        Token::SOF          => 'T_OPEN_TAG',
+        Token::STRING       => 'T_STRING',
+    ];
+
+    /**
+     * Defines how special keywords are mapped to PHP token types
+     *
+     * @var array
+     */
+    private $keywordTokenTypeMap = [
+        'enum'       => 'T_CLASS',
+        'extend'     => 'T_EXTENDS', //TODO This might not be the appropriate equivalent
+        'interface'  => 'T_INTERFACE',
+        'implements' => 'T_IMPLEMENTS',
+        'type'       => 'T_CLASS',
+        'union'      => 'T_CLASS',
+        //TODO Add further types
     ];
 
     /**
@@ -27,17 +71,13 @@ class GraphQL extends JS
      * @param string $content
      * @param Config $config
      * @param string $eolChar
-     * @throws \PHP_CodeSniffer\Exceptions\TokenizerException
+     * @throws TokenizerException
      */
     public function __construct($content, Config $config, $eolChar = '\n')
     {
-        //add our token values
-        $this->tokenValues = array_merge(
-            $this->tokenValues,
-            $this->additionalTokenValues
-        );
+        //TODO We might want to delete this unless we need the constructor to work totally different
 
-        //let parent do its job (which will start tokenizing)
+        //let parent do its job
         parent::__construct($content, $config, $eolChar);
     }
 
@@ -46,7 +86,110 @@ class GraphQL extends JS
      */
     public function processAdditional()
     {
-        //NOP Does nothing intentionally
+        //NOP: Does nothing intentionally
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @throws TokenizerException
+     */
+    protected function tokenize($string)
+    {
+        $this->logVerbose('*** START GRAPHQL TOKENIZING ***');
+
+        $string = str_replace($this->eolChar, "\n", $string);
+        $tokens = [];
+        $lexer  = new Lexer(
+            new Source($string)
+        );
+
+        do {
+            $kind  = $lexer->token->kind;
+            $value = $lexer->token->value ?: '';
+
+            //if we have encountered a keyword, we convert it
+            //otherwise we translate the token or default it to T_STRING
+            if ($kind === Token::NAME && isset($this->keywordTokenTypeMap[$value])) {
+                $tokenType = $this->keywordTokenTypeMap[$value];
+            } elseif (isset($this->tokenTypeMap[$kind])) {
+                $tokenType = $this->tokenTypeMap[$kind];
+            } else {
+                $tokenType = 'T_STRING';
+            }
+
+            //some GraphQL tokens need special handling
+            switch ($kind) {
+                case Token::AT:
+                case Token::BRACE_L:
+                case Token::BRACE_R:
+                case Token::PAREN_L:
+                case Token::PAREN_R:
+                    $value = $kind;
+                    break;
+                default:
+                    //NOP
+            }
+
+            //finally we create the PHP token
+            $token = [
+                'code'    => constant($tokenType),
+                'type'    => $tokenType,
+                'content' => $value,
+            ];
+            $line  = $lexer->token->line;
+
+            $lexer->advance();
+
+            //if line has changed (and we're not on start of file) we have to append at least one line break to current
+            //tokens content otherwise PHP_CodeSniffer will screw up line numbers
+            if ($lexer->token->line !== $line && $kind !== Token::SOF) {
+                $token['content'] .= $this->eolChar;
+            }
+            $tokens[] = $token;
+            $tokens = array_merge(
+                $tokens,
+                $this->getNewLineTokens($line, $lexer->token->line)
+            );
+        } while ($lexer->token->kind !== Token::EOF);
+
+        $this->logVerbose('*** END GRAPHQL TOKENIZING ***');
+        return $tokens;
+    }
+
+    /**
+     * Returns tokens of empty new lines for the range <var>$lineStart</var> to <var>$lineEnd</var>
+     *
+     * @param int $lineStart
+     * @param int $lineEnd
+     * @return array
+     */
+    private function getNewLineTokens($lineStart, $lineEnd)
+    {
+        $amount = ($lineEnd - $lineStart) - 1;
+        $tokens = [];
+
+        for ($i = 0; $i < $amount; ++$i) {
+            $tokens[] = [
+                'code'    => T_WHITESPACE,
+                'type'    => 'T_WHITESPACE',
+                'content' => $this->eolChar,
+            ];
+        }
+
+        return $tokens;
+    }
+
+    /**
+     * Logs <var>$message</var> if {@link PHP_CODESNIFFER_VERBOSITY} is greater than <var>$level</var>.
+     *
+     * @param string $message
+     * @param int $level
+     */
+    private function logVerbose($message, $level = 1)
+    {
+        if (PHP_CODESNIFFER_VERBOSITY > $level) {
+            printf("\t%s" . PHP_EOL, $message);
+        }
+    }
 }
