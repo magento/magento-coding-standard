8244534a62b8e1dc38622f904f47c6f6feac1369
Lena Orobei <oorobei@magento.com>
2020-02-10 11:08:20 -0600

    Fixed confusing message

--- Magento2/Sniffs/Commenting/ClassAndInterfacePHPDocFormattingSniff.php
+++ Magento2/Sniffs/Commenting/ClassAndInterfacePHPDocFormattingSniff.php
@@ -65,7 +65,7 @@ class ClassAndInterfacePHPDocFormattingSniff implements Sniff
         if ($this->PHPDocFormattingValidator->providesMeaning($namePtr, $commentStartPtr, $tokens) !== true) {
             $phpcsFile->addWarning(
                 sprintf(
-                    '%s description must contain meaningful information beyond what the class name provides or be removed.',
+                    '%s description must contain meaningful information beyond what its name provides or be removed.',
                     ucfirst($tokens[$stackPtr]['content'])
                 ),
                 $stackPtr,
