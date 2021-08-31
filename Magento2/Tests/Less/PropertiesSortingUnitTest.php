3ebe3979890597314f2405b64a6f27f6e904822b
Marc Ginesta <mginesta@adobe.com>
2021-08-31 15:27:21 +0200

    AC-949: Create unit test for Magento2\Less\PropertiesSortingSniff check

---
+++ Magento2/Tests/Less/PropertiesSortingUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento2\Tests\Less;
+
+class PropertiesSortingUnitTest extends AbstractLessSniffUnitTestCase
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [
+            9 => 1,
+            26 => 1
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
