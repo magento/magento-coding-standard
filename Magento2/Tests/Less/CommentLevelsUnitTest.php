cf9c0dd7d21e0b283085d5ac99cf081c1fb3a348
Magento Community Engineering <31669971+magento-engcom-team@users.noreply.github.com>
2021-08-31 10:01:55 -0500

    [Imported] AC-945: Create unit test for Magento2\Less\CommentLevelsSniff check (#34)
    https://jira.corp.magento.com/browse/AC-945

---
+++ Magento2/Tests/Less/CommentLevelsUnitTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento2\Tests\Less;
+
+class CommentLevelsUnitTest extends AbstractLessSniffUnitTestCase
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [
+            16 => 1,
+            30 => 1,
+            31 => 1,
+            44 => 1,
+            46 => 1,
+            47 => 1
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
