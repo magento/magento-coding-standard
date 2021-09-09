8b2cd9041f1337aec04c28519984fc6eb5f7afc7
Elisea Cornejo <ecornejo@magento.com>
2021-09-09 11:35:52 +0200

    AC-666: Create phpcs static check for CopyrightTest

---
+++ Magento2/Sniffs/Legacy/CopyrightSniff.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types = 1);
+
+namespace Magento2\Sniffs\Legacy;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class CopyrightSniff implements Sniff
+{
+    private const WARNING_CODE = 'FoundCopyrightMissingOrWrongFormat';
+    
+    private const COPYRIGHT_MAGENTO_TEXT = 'Copyright © Magento, Inc. All rights reserved.';
+    private const COPYRIGHT_ADOBE = '/Copyright \d+ Adobe/';
+    
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_OPEN_TAG];
+    }
+    
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $positionOpenTag = $phpcsFile->findPrevious(T_OPEN_TAG, $stackPtr - 1);
+       
+        if ($positionOpenTag === false){
+            $positionComment = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $stackPtr);
+            $contentFile = $phpcsFile->getTokens()[$positionComment]['content'];
+            $adobeCopyrightFound = preg_match(self::COPYRIGHT_ADOBE, $contentFile);
+            
+            if (strpos($contentFile, self::COPYRIGHT_MAGENTO_TEXT) === false || $adobeCopyrightFound === false) {
+                $phpcsFile->addWarningOnLine(
+                    'Copyright is missing or has wrong format',
+                    null,
+                    self::WARNING_CODE
+                );
+            }
+        }
+    }
+}
---
+++ Magento2/Tests/Legacy/CopyrightUnitTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento2\Tests\Legacy;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class CopyrightUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList($testFile = ''): array
+    {
+        if ($testFile === 'CopyrightUnitTest.4.inc') {
+            return [];
+        }
+        
+        return [
+            null => 1,
+        ];
+    }
+}
