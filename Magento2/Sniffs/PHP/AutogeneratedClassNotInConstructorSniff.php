a6d2530c8eb5300ee7d79f2d0bc3f05ac6d7b902
Jorge Cuerdo Álvarez <jocual@gmail.com>
2021-09-14 16:20:45 +0200

    AC-662: Create phpcs static check for AutogeneratedClassNotInConstructorTest

--- Magento2/Sniffs/PHP/AutogeneratedClassNotInConstructorSniff.php
+++ Magento2/Sniffs/PHP/AutogeneratedClassNotInConstructorSniff.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright © Magento. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types = 1);
 
 namespace Magento2\Sniffs\PHP;
 
