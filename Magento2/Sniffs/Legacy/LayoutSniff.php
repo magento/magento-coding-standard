a78b21dbdff8873078a44cb16859bc3cbaa16ab6
Sergio Vera <sergio.vera@gmail.com>
2021-10-13 14:58:41 +0200

    Update Magento2/Sniffs/Legacy/LayoutSniff.php
    Co-authored-by: Sergii Ivashchenko <serg.ivashchenko@gmail.com>

--- Magento2/Sniffs/Legacy/LayoutSniff.php
+++ Magento2/Sniffs/Legacy/LayoutSniff.php
@@ -271,7 +271,7 @@ class LayoutSniff implements Sniff
     {
         $doc = new DomDocument('1.0');
         $doc->formatOutput = true;
-        $doc->loadXML($phpcsFile->getTokensAsString(0, 999999));
+        $doc->loadXML($phpcsFile->getTokensAsString(0, count($phpcsFile->getTokens())));
         return $doc->saveXML();
     }
 
