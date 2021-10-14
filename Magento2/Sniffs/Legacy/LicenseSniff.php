7fdd6b44b43c44a5a60272b531c39e9ebc8c2835
Elisea Cornejo <ecornejo@magento.com>
2021-10-14 11:17:31 +0200

    AC-672: Create phpcs static check for LicenseTest

---
+++ Magento2/Sniffs/Legacy/LicenseSniff.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types = 1);
+
+namespace Magento2\Sniffs\Legacy;
+
+use Magento2\Sniffs\Less\TokenizerSymbolsInterface;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class LicenseSniff implements Sniff
+{
+    private const WARNING_CODE = 'FoundLegacyTextInCopyright';
+    
+    public $supportedTokenizers = [TokenizerSymbolsInterface::TOKENIZER_CSS, 'PHP'];
+    
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [
+            T_DOC_COMMENT_STRING,
+            T_INLINE_HTML
+        ];
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $content = null;
+        
+        if ($tokens[$stackPtr]['code'] === T_DOC_COMMENT_STRING) {
+            $content = $tokens[$stackPtr]['content'];
+        }
+        if ($tokens[$stackPtr]['code'] === T_INLINE_HTML) {
+            $content = $phpcsFile->getTokensAsString($stackPtr, 1);
+        }
+        if ($content != null){
+            $this->checkLicense($content, $stackPtr, $phpcsFile); 
+        }
+    }
+
+    /**
+     * @param string $content
+     * @param int $stackPtr
+     * @param File $phpcsFile
+     */
+    private function checkLicense(string $content, int $stackPtr, File $phpcsFile): void
+    {
+        $commentContent = $content;
+        if (stripos($commentContent, 'copyright') === false) {
+            return;
+        }
+        foreach (['Irubin Consulting Inc', 'DBA Varien', 'Magento Inc'] as $legacyText) {
+            if (stripos($commentContent, $legacyText) !== false) {
+                $phpcsFile->addWarning(
+                    sprintf("The copyright license contains legacy text: %s.", $legacyText),
+                    $stackPtr,
+                    self::WARNING_CODE
+                );
+            }
+        }
+    }
+}
