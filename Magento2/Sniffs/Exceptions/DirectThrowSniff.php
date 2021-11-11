483de85f2d3d38b9755e2db2051a45f09fca1645
MeenakshiSundaram <msundara@adobe.com>
2021-11-11 21:03:16 +0530

    fix custom exception gives false positive with DirectThrowSniff

--- Magento2/Sniffs/Exceptions/DirectThrowSniff.php
+++ Magento2/Sniffs/Exceptions/DirectThrowSniff.php
@@ -44,19 +44,29 @@ class DirectThrowSniff implements Sniff
         $tokens = $phpcsFile->getTokens();
         $endOfStatement = $phpcsFile->findEndOfStatement($stackPtr);
         $posOfException = $phpcsFile->findNext(T_STRING, $stackPtr, $endOfStatement);
-        $content = $tokens[$posOfException]['content'];
-        $exceptionClassInUseStatement = false;
+
+        $fullExceptionString = $this->getFullClassNameAndAlias($tokens, $stackPtr, $endOfStatement);
+        $exceptionString = 'Exception';
+        $customExceptionFound = false;
         foreach ($tokens as $key => $token) {
             if ($token['code'] === T_USE) {
                 $endOfUse = $phpcsFile->findEndOfStatement($key);
-                $posOfException = $phpcsFile->findNext(T_STRING, $key, $key + 3, false, 'Exception');
-                if ($posOfException && $phpcsFile->findNext(T_SEMICOLON, $posOfException+1, $endOfUse + 1)) {
-                    $exceptionClassInUseStatement = true;
-                    break;
+                $useStatementValue = $this->getFullClassNameAndAlias($tokens, $key, $endOfUse);
+                //we safely consider use statement has alias will not be a direct exception class
+                if (empty($useStatementValue['alias'])) {
+                    if (substr($useStatementValue['name'], 0, strlen($exceptionString)) !== $exceptionString
+                        && substr($useStatementValue['name'], -strlen($exceptionString)) === $exceptionString
+                        && $useStatementValue['name'] !== $exceptionString
+                    ) {
+                        $customExceptionFound = true;
+                        break;
+                    }
                 }
             }
         }
-        if ($content === '\Exception' || ($content === 'Exception' && $exceptionClassInUseStatement)) {
+        if (($tokens[$posOfException]['content'] === 'Exception' && !$customExceptionFound)
+            || $fullExceptionString['name'] === '\Exception'
+        ) {
             $phpcsFile->addWarning(
                 $this->warningMessage,
                 $stackPtr,
@@ -64,5 +74,34 @@ class DirectThrowSniff implements Sniff
                 [$posOfException]
             );
         }
+    }
+
+    /**
+     * Get full class name and alias
+     *
+     * @param array $tokens
+     * @param int $start
+     * @param int $end
+     * @return array
+     */
+    private function getFullClassNameAndAlias($tokens, $start, $end)
+    {
+        $fullName = $alias = '';
+        $foundAlias = false;
+        for ($i = $start; $i <= $end; $i++) {
+            $type = $tokens[$i]['code'];
+            if ($type === T_AS) {
+                $foundAlias = true;
+                continue;
+            }
+            if ($type === T_STRING || $type === T_NS_SEPARATOR) {
+                if (!$foundAlias) {
+                    $fullName .= $tokens[$i]['content'];
+                } else {
+                    $alias = $tokens[$i]['content'];
+                }
+            }
+        }
+        return ['name' => $fullName, 'alias' => $alias];
     }
 }
