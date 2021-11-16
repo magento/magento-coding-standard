b669d8c22c4af75a6ef67a6335bf1c047b81467c
Elisea Cornejo <ecornejo@magento.com>
2021-11-16 15:51:02 +0100

    AC-1740: Add rectorphp as a dependency to magento-coding-standard

--- rector.php
+++ rector.php
@@ -15,8 +15,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
     $containerConfigurator->import(SetList::PHP_81);
 
