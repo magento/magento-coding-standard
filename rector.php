d463a5aebf3b928da12a1ec6dd2721dedebd2930
Elisea Cornejo <ecornejo@magento.com>
2021-11-19 15:23:26 +0100

    AC-1740: Add rectorphp as a dependency to magento-coding-standard and run it as part of the GitHub actions

--- rector.php
+++ rector.php
@@ -4,24 +4,11 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
-use Rector\Php80\Rector\FuncCall\ClassOnObjectRector;
-use Rector\Php80\Rector\FuncCall\Php8ResourceReturnToObjectRector;
-use Rector\Php80\Rector\FuncCall\TokenGetAllToObjectRector;
-use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
-use Rector\Php80\Rector\Identical\StrEndsWithRector;
-use Rector\Php80\Rector\Identical\StrStartsWithRector;
-use Rector\Php80\Rector\NotIdentical\StrContainsRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
-use Rector\Php80\Rector\Ternary\GetDebugTypeRector;
-use Rector\Php81\Rector\Class_\MyCLabsClassToEnumRector;
-use Rector\Php81\Rector\Class_\SpatieEnumClassToEnumRector;
-use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
-use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
-use Rector\Php81\Rector\FunctionLike\IntersectionTypesRector;
-use Rector\Php81\Rector\MethodCall\MyCLabsMethodCallToEnumConstRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\SetList;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php80\Rector\ClassMethod\FinalPrivateToPrivateVisibilityRector;
+use Rector\Php80\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
+use Rector\Php80\Rector\ClassMethod\SetStateToStaticRector;
+use Rector\Php81\Rector\FuncCall\Php81ResourceReturnToObjectRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -36,34 +23,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
-    
-    $parameters->set(Option::SKIP, [
-        ReadOnlyPropertyRector::class, 
-        StrContainsRector::class,
-        UnionTypesRector::class,
-        StrStartsWithRector::class,
-        StrEndsWithRector::class,
-        FinalizePublicClassConstantRector::class,
-        RemoveUnusedVariableInCatchRector::class,
-        ClassOnObjectRector::class,
-        Php8ResourceReturnToObjectRector::class,
-        TokenGetAllToObjectRector::class,
-        GetDebugTypeRector::class,
-        ChangeSwitchToMatchRector::class,
-        NewInInitializerRector::class,
-        IntersectionTypesRector::class,
-        MyCLabsMethodCallToEnumConstRector::class,
-        MyCLabsClassToEnumRector::class,
-        SpatieEnumClassToEnumRector::class
-    ]);
-
-    // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
 
     // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
+    $services = $containerConfigurator->services();
 
     // register a single rule
-    // $services->set(TypedPropertyRector::class);
+    $services->set(FinalPrivateToPrivateVisibilityRector::class);
+    $services->set(OptionalParametersAfterRequiredRector::class);
+    $services->set(SetStateToStaticRector::class);
+    $services->set(StringableForToStringRector::class);
+    $services->set(Php81ResourceReturnToObjectRector::class);
 };
