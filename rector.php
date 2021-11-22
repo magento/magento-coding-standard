4b3df3e6a690de4cddd3572387e2130d4cd99072
Elisea Cornejo <ecornejo@magento.com>
2021-11-22 16:11:36 +0100

    AC-1740: Add rectorphp as a dependency to magento-coding-standard and run it as part of the GitHub actions

--- rector.php
+++ rector.php
@@ -14,12 +14,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/Magento2',
-        __DIR__ . '/Magento2Framework',
-        __DIR__ . '/PHP_CodeSniffer'
-    ]);
-    $parameters->set(Option::BOOTSTRAP_FILES, [__DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php']);
 
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
