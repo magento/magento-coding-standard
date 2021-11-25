5157584b5a3ca1b78da8a56bf77f74736ef816f8
Sergio Vera <svera@adobe.com>
2021-11-25 17:15:12 +0100

    AC-1060 move relevant sniffs from phpcompatibility (#130)
    * AC-1060: Move relevant sniffs from PHPCompatibility

---
+++ Magento2/Tests/PHPCompatibility/NewMagicClassConstantUnitTest.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace Magento2\Tests\PHPCompatibility;
+
+
+/**
+ * Test the NewMagicClassConstant sniff.
+ *
+ * @group newMagicClassConstant
+ * @group constants
+ *
+ * @covers \Magento2\Sniffs\PHPCompatibility\NewMagicClassConstantSniff
+ *
+ * @since 7.1.4
+ */
+class NewMagicClassConstantUnitTest extends BaseSniffTest
+{
+
+    /**
+     * Test correctly identifying the magic class constant.
+     *
+     * @dataProvider dataNewMagicClassConstant
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNewMagicClassConstant($line)
+    {
+        $file = $this->sniffFile(__FILE__, '5.4');
+        $this->assertError($file, $line, 'The magic class constant ClassName::class was not available in PHP 5.4 or earlier');
+
+        $file = $this->sniffFile(__FILE__, '5.5');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNewMagicClassConstant()
+     *
+     * @return array
+     */
+    public function dataNewMagicClassConstant()
+    {
+        return [
+            [6],
+            [12],
+            [27],
+            [28],
+            [29],
+            [30],
+            [31],
+            [32],
+        ];
+    }
+
+
+    /**
+     * Verify that no false positives are thrown for valid code.
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(__FILE__, '5.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return [
+            [4],
+            [10],
+            [18],
+            [19],
+        ];
+    }
+
+
+    /**
+     * Test correctly identifying the magic class constant when used on instantiated objects.
+     *
+     * @dataProvider dataNewMagicClassConstantOnObject
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNewMagicClassConstantOnObject($line)
+    {
+        $file = $this->sniffFile(__FILE__, '7.4');
+        $this->assertError($file, $line, 'Using the magic class constant ::class with dynamic class names is not supported in PHP 7.4 or earlier');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNewMagicClassConstantOnObject()
+     *
+     * @return array
+     */
+    public function dataNewMagicClassConstantOnObject()
+    {
+        return [
+            [35],
+            [36],
+            [37],
+            [38],
+            [39],
+
+            // Still not supported, but throwing an error anyhow.
+            [49],
+            [50],
+            [51],
+            [52],
+        ];
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '8.0');
+        $this->assertNoViolation($file);
+    }
+}
---
+++ Magento2/Tests/PHPCompatibility/NewTrailingCommaUnitTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace Magento2\Tests\PHPCompatibility;
+
+/**
+ * Test the NewTrailingComma sniff.
+ *
+ * @group newTrailingComma
+ * @group syntax
+ *
+ * @covers \Magento2\Sniffs\PHPCompatibility\NewTrailingCommaSniff
+ *
+ * @since 10.0.0
+ */
+class NewTrailingCommaUnitTest extends BaseSniffTest
+{
+
+    /**
+     * Test correctly identifying trailing comma's in function declarations.
+     *
+     * @dataProvider dataTrailingComma
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testTrailingComma($line)
+    {
+        $file = $this->sniffFile(__FILE__, '7.4');
+        $this->assertError($file, $line, "Trailing comma's are not allowed in function declaration parameter lists in PHP 7.4 or earlier");
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testTrailingComma()
+     *
+     * @return array
+     */
+    public function dataTrailingComma()
+    {
+        return [
+            [39],
+            [44],
+            [48],
+            [59],
+            [75],
+            [89],
+            [94],
+        ];
+    }
+
+
+    /**
+     * Test correctly identifying trailing comma's in closure use lists.
+     *
+     * @dataProvider dataTrailingCommaClosureUse
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testTrailingCommaClosureUse($line)
+    {
+        $file = $this->sniffFile(__FILE__, '7.4');
+        $this->assertError($file, $line, "Trailing comma's are not allowed in closure use lists in PHP 7.4 or earlier");
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testTrailingCommaClosureUse()
+     *
+     * @return array
+     */
+    public function dataTrailingCommaClosureUse()
+    {
+        return [
+            [68],
+            [79],
+            [91],
+        ];
+    }
+
+
+    /**
+     * Verify the sniff does not throw false positives for valid code.
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(__FILE__, '7.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        // No errors expected on the first 33 lines.
+        $data = [];
+        for ($line = 1; $line <= 33; $line++) {
+            $data[] = [$line];
+        }
+
+        $data[] = [97];
+        $data[] = [101];
+
+        return $data;
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '8.0');
+        $this->assertNoViolation($file);
+    }
+}
