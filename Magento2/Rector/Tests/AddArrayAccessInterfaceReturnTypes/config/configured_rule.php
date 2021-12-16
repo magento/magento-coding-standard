1be74dc062a66603b0dd310755bf456c3430e818
m2-github-services <87783418+m2-github-services@users.noreply.github.com>
2021-12-16 06:18:14 -0800

    [Imported] AC-1718 return types for arrayaccess methods (#138)
    * AC-1717: Passing null to non-nullable internal function parameters is deprecated

---
+++ Magento2/Rector/Tests/AddArrayAccessInterfaceReturnTypes/config/configured_rule.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright 2021 Adobe
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento2\Rector\Src\AddArrayAccessInterfaceReturnTypes;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(AddArrayAccessInterfaceReturnTypes::class);
+};
