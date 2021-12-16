3230fbb6f23d6388d2b5504860980c8820a5e379
m2-github-services <87783418+m2-github-services@users.noreply.github.com>
2021-12-16 02:58:40 -0800

    [Imported] AC-1717: Passing null to non-nullable internal function parameters is… (#136)
    * AC-1717: Passing null to non-nullable internal function parameters is deprecated

---
+++ Magento2/Rector/Src/ReplaceMbStrposNullLimit.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright 2021 Adobe
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento2\Rector\Src;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Scalar\LNumber;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\Exception\PoorDocumentationException;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class ReplaceMbStrposNullLimit extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (!$this->isName($node->name, 'mb_strpos')) {
+            return null;
+        }
+
+        if ($node->args[2] !== LNumber::class) {
+            $node->args[2] = $this->nodeFactory->createArg(0);
+            return $node;
+        }
+
+        return null;
+    }
+
+    /**
+     * @return RuleDefinition
+     * @throws PoorDocumentationException
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Change mb_strpos offset from null to 0', [
+                new CodeSample(
+                    'mb_strpos("pattern", "subject", null, "encoding");',
+                    'mb_strpos("pattern", "subject", 0, "encoding");'
+                ),
+            ]
+        );
+    }
+}
