e34145508a53a2cbf66f7616bf3cc453db2be15b
andrii.zinkevych <a.zinkevych@atwix.com>
2022-01-25 12:11:18 +0200

    AC-1999: Static test to check void closing tags

---
+++ Magento2/Tests/Html/HtmlClosingVoidTagsUnitTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento2\Tests\Html;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class HtmlClosingVoidTagsUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * @inheritdoc
+     */
+    public function getErrorList()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getWarningList()
+    {
+        return [1 => 15];
+    }
+}
