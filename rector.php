df25dabc17e357e27508232eabcbb957b4a9bc53
Rajesh Kumar <glo71317@adobe.com>
2024-03-18 18:46:51 +0530

    Fixed tests after update the latest versions

--- rector.php
+++ rector.php
@@ -18,15 +18,15 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
     // get services (needed for register a single rule)
-    $services = $rectorConfig->services();
+//    $services = $rectorConfig->services();
 
     // register a single rule
-    $services->set(FinalPrivateToPrivateVisibilityRector::class);
-    $services->set(OptionalParametersAfterRequiredRector::class);
-    $services->set(SetStateToStaticRector::class);
-    $services->set(StringableForToStringRector::class);
-    $services->set(Php81ResourceReturnToObjectRector::class);
-    $services->set(ReplacePregSplitNullLimit::class);
-    $services->set(ReplaceMbStrposNullLimit::class);
-    $services->set(ReplaceNewDateTimeNull::class);
+    $rectorConfig->singleton(FinalPrivateToPrivateVisibilityRector::class);
+    $rectorConfig->singleton(OptionalParametersAfterRequiredRector::class);
+    $rectorConfig->singleton(SetStateToStaticRector::class);
+    $rectorConfig->singleton(StringableForToStringRector::class);
+    $rectorConfig->singleton(Php81ResourceReturnToObjectRector::class);
+    $rectorConfig->singleton(ReplacePregSplitNullLimit::class);
+    $rectorConfig->singleton(ReplaceMbStrposNullLimit::class);
+    $rectorConfig->singleton(ReplaceNewDateTimeNull::class);
 };
