388a93852ec575f396f22a89c8e36b2667965f09
jayrangnani-gl <142883278+jayrangnani-gl@users.noreply.github.com>
2024-08-16 19:05:35 +0530

    AC-9098:Ability to use Adobe copyright for Adobe Commerce source code for Magento Open Source (CE)-Validation

--- Magento2Framework/Sniffs/Header/CopyrightAnotherExtensionsFilesSniff.php
+++ Magento2Framework/Sniffs/Header/CopyrightAnotherExtensionsFilesSniff.php
@@ -12,11 +12,8 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 
 class CopyrightAnotherExtensionsFilesSniff implements Sniff
 {
+    use CopyrightValidation;
     private const WARNING_CODE = 'FoundCopyrightMissingOrWrongFormat';
-
-    private const COPYRIGHT_MAGENTO_TEXT = 'Copyright © Magento, Inc. All rights reserved.';
-    private const COPYRIGHT_ADOBE = '/Copyright \d+ Adobe/';
-    private const COPYRIGHT_ADOBE_TEXT = 'ADOBE CONFIDENTIAL';
 
     /**
      * Defines the tokenizers that this sniff is using.
@@ -45,19 +42,15 @@ class CopyrightAnotherExtensionsFilesSniff implements Sniff
             return;
         }
 
-        $fileText = $phpcsFile->getTokensAsString($stackPtr, count($phpcsFile->getTokens()));
+      // @phpcs:ignore Magento2.Functions.DiscouragedFunction.Discouraged
+        $content = file_get_contents($phpcsFile->getFilename());
 
-        if (strpos($fileText, self::COPYRIGHT_MAGENTO_TEXT) !== false
-            || preg_match(self::COPYRIGHT_ADOBE, $fileText)
-                || strpos($fileText, self::COPYRIGHT_ADOBE_TEXT) !== false
-        ) {
-            return;
+        if ($this->isCopyrightYearValid($content) === false) {
+            $phpcsFile->addWarningOnLine(
+                'Copyright is missing or has wrong format',
+                null,
+                self::WARNING_CODE
+            );
         }
-
-        $phpcsFile->addWarningOnLine(
-            'Copyright is missing or has wrong format',
-            null,
-            self::WARNING_CODE
-        );
     }
 }
--- Magento2Framework/Sniffs/Header/CopyrightGraphQLSniff.php
+++ Magento2Framework/Sniffs/Header/CopyrightGraphQLSniff.php
@@ -12,11 +12,8 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 
 class CopyrightGraphQLSniff implements Sniff
 {
+    use CopyrightValidation;
     private const WARNING_CODE = 'FoundCopyrightMissingOrWrongFormat';
-
-    private const COPYRIGHT_MAGENTO_TEXT = 'Copyright © Magento, Inc. All rights reserved.';
-    private const COPYRIGHT_ADOBE = '/Copyright \d+ Adobe/';
-    private const COPYRIGHT_ADOBE_TEXT = 'ADOBE CONFIDENTIAL';
 
     private const FILE_EXTENSION = 'graphqls';
 
@@ -45,16 +42,12 @@ class CopyrightGraphQLSniff implements Sniff
         // @phpcs:ignore Magento2.Functions.DiscouragedFunction.Discouraged
         $content = file_get_contents($phpcsFile->getFilename());
 
-        if (strpos($content, self::COPYRIGHT_MAGENTO_TEXT) !== false
-            || preg_match(self::COPYRIGHT_ADOBE, $content)
-                || strpos($content, self::COPYRIGHT_ADOBE_TEXT) !== false) {
-            return;
+        if ($this->isCopyrightYearValid($content) === false) {
+            $phpcsFile->addWarningOnLine(
+                'Copyright is missing or has wrong format',
+                null,
+                self::WARNING_CODE
+            );
         }
-
-        $phpcsFile->addWarningOnLine(
-            'Copyright is missing or has wrong format',
-            null,
-            self::WARNING_CODE
-        );
     }
 }
